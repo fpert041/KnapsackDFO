e91b420cbd2f735653f5c728185cce0408a07374
agapesoft <mu301fp@gold.ac.uk>
2017-12-13 13:10:50 +0000

    fran folder

---
+++ experiments/francesco/experimental_modules.cpp
@@ -0,0 +1,8 @@
+//
+//  experimental_modules.cpp
+//  imgSymmetry
+//
+//  Created by Francesco Perticarari on 13/12/2017.
+//
+
+#include "experimental_modules.hpp"
---
+++ experiments/francesco/experimental_modules.hpp
@@ -0,0 +1,13 @@
+//
+//  experimental_modules.hpp
+//  imgSymmetry
+//
+//  Created by Francesco Perticarari on 13/12/2017.
+//
+
+#ifndef experimental_modules_hpp
+#define experimental_modules_hpp
+
+#include <stdio.h>
+
+#endif /* experimental_modules_hpp */
